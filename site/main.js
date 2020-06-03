26001455ddada5d2deb95c3da458c82ad7c23508
Garlicvideos <Garlicvideos@gmail.com>
2020-06-03 23:24:23 +0800

    Rework landing and added new sections
    - Landing page looks vastly different
    - Changed classes of display cards to be more globally-oriented
    - Added incomplete showcase section
    - Removed inital showcase at introduction

--- site/main.js
+++ site/main.js
@@ -1,14 +1,7 @@
-$(document).ready(function() {
-	//Chevron animation
-	$("#view-work-btn").on("mouseenter", function() {
-		$(this).find(".gg-chevron-right").css("left", "4%");
-	});
+$(document).ready(() => {
 
-	$("#view-work-btn").on("mouseleave", function() {
-		$(this).find(".gg-chevron-right").css("left", "0");
-	});
-
-	$(window).scroll(function (event) {
+	//Navbar scroll background updates
+	$(window).scroll((event) => {
 	    var scroll = $(window).scrollTop();
 	    //Navbar background 1
 	    if (scroll > 100) {
@@ -20,13 +13,52 @@ $(document).ready(function() {
 	    }
 
 	    //Navbar background 2
-	    if (scroll > 670) {
+	    if (scroll > 720) {
 	    	$("#nav-bg2").addClass("show-bg");
 	    }
 
-	    if (scroll < 670 && scroll > 100) {
+	    if (scroll < 720 && scroll > 100) {
 	    	$("#nav-bg2").removeClass("show-bg");
 	    	$("#nav-bg").addClass("show-bg");
 	    }
+	});
+
+	//Chevron animations
+	$(".chev-anim").on("mouseenter", function() {
+		$(this).find(".gg-chevron-right").css("left", "4%");
+	});
+
+	$(".chev-anim").on("mouseleave", function() {
+		$(this).find(".gg-chevron-right").css("left", "0");
+	});
+
+	//Card shell hover animation
+	$(".card-shell").on("mouseenter", function() {
+		//Dim background color
+		console.log($(this).css("background-color"));
+		if ($(this).hasClass("card-highlight")) {
+			$(this).css("background-color", "#4d4702");
+		} else {
+			$(this).css("background-color", "#595959");
+		}
+		//Blur the card
+		$(this).addClass("card-blur");
+		//Show the overlay text
+		$(this).find(".card-overlay").css("display", "flex");
+	});
+
+	//Card shell unhover animation
+	$(".card-shell").on("mouseleave", function() {
+		//Brighten background color
+		console.log($(this).css("background-color"));
+		if ($(this).hasClass("card-highlight")) {
+			$(this).css("background-color", "#f0e005");
+		} else {
+			$(this).css("background-color", "#e8e8e8");
+		}
+		//Unblur the card
+		$(this).removeClass("card-blur");
+		//Hide the overlay text
+		$(this).find(".card-overlay").css("display", "none");
 	});
 });
